8a94f082c00daacf99b1a9d677d0accb0d971012
ArmandoFC <fc.armando@outlook.com>
2026-07-13 17:08:57 -0600

    cambio rama

--- general/views.py
+++ general/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-#from django.contrib.auth.decorators import login_required
+#from django.contrib.auth.decorators import login_required actualizacion
 from .models import TipoCambio # Importamos tu modelo
 from decimal import Decimal
 import datetime
